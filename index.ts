c55abc1a0ec50da9f618a0cd4b5d6570da02cabe
lunarpalm <22187334+lunarpalm@users.noreply.github.com>
2024-05-15 02:26:00 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './src/App';
 import store from './src/app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './src/serviceWorker';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
