e827976d0ca76b5e0335bca53d7e80784422ccf5
lunarpalm <22187334+lunarpalm@users.noreply.github.com>
2024-05-15 02:53:16 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './src/App';
 import store from './src/app/store';
 import { Provider } from 'react-redux';
 
-const root = createRoot(document.getElementById("root"));
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Initial render
 root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
