928f5a9b34de3d57612cc8af3b3cbbca60540405
lunarpalm <22187334+lunarpalm@users.noreply.github.com>
2024-05-15 02:46:19 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,7 +4,6 @@ import './index.css';
 import App from './src/App';
 import store from './src/app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './src/serviceWorker';
 
 const root = createRoot(document.getElementById("root"));
 
@@ -14,8 +13,3 @@ root.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
